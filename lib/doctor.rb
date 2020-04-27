fcb8796f25fa28810f88e7b71a70ac10a2b493cb
John Carter <johncarter352@gmail.com>
2020-04-27 01:19:39 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,26 @@
+class Doctor
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self 
+  end
+  
+  def self.all 
+    @@all 
+  end
+  
+  def new_appointment
+  end 
+  
+  def appointments
+    Appointment.all.select{|doctor| appointments.doctor == self}
+  end 
+  
+  def patients
+  end 
+  
+  
+end
---
+++ lib/patient.rb
@@ -0,0 +1,21 @@
+class Patient
+  attr_accessor :name 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self
+  end
+  
+  def new_appointment
+  end 
+  
+  def appointments 
+  end 
+  
+  def doctors 
+  end 
+  
+  
+end
